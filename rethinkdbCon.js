c0984df1e1bc918cadb3c37752746a336604d683
Harleen Kaur <harleen.kaur212@gmail.com>
2017-08-22 12:48:45 +0530

    type(fix): added table name and run() into a rethinkdbCon.js

--- rethinkdbCon.js
+++ rethinkdbCon.js
@@ -1,2 +1,7 @@
 // require the rethinkdbdash Module
 var r = require('rethinkdbdash')();
+
+//save the data into a table called as 'agents'
+r.table('agents').run().then(function(result) {
+  console.log("result", result);
+});
