7e27ab067894c1946747a0b8d1b838d75be93de5
Harleen Kaur <harleen.kaur212@gmail.com>
2017-08-25 11:15:22 +0530

    type(fix): rewrite the code for random.json API on the basis of countryCode

--- index.js
+++ index.js
@@ -1,168 +1,43 @@
-// // require the GOT Module
-// const got = require('got');
+const reThinkPlus = require('rethink-plus')
+var superagent = require('superagent')
+const dbData = new reThinkPlus({
+  })
+async function getDetails(){
+try{
+  var details = await superagent.get('http://pplapi.com/random.json');
+  var response = details.body;
+   var inserted = await dbData.db('randomdata').table('datadetails').insert(response).run();
+  return inserted;
+}
+ catch(e){
+   console.log(e)
+ }
+}
+getDetails().then(function(Data){
+    //console.log('Data',Data);
+})
 
-// // Use got to call the API http://pplapi.com/random.json to get the random data and save it to randomData database in table Agents
-// got('http://pplapi.com/random.json')
-//     .then(response => {
-//     body =JSON.parse(response.body);
-//     var userDetails = {
-//           neuroticism: body.neuroticism,
-//           language: body.language,
-//           country_tld: body.country_tld,
-//           age: body.age,
-//           income: body.income,
-//           longitude: body.longitude,
-//           sex: body.sex,
-//           religion: body.religion,
-//           extraversion: body.extraversion,
-//           date_of_birth: body.date_of_birth,
-//           agreeableness: body.agreeableness,
-//           id_str: body.id_str,
-//           conscientiousness: body.conscientiousness,
-//           internet: body.internet,
-//           country_name :body.country_name,
-//           openness: body.openness,
-//           id: body.id
-//         };
-//     })
-//     .catch(error => {
-//         console.log(error.response.body);
-//     });
-
-// // Use got to call the API http://pplapi.com/country/us/random.json to get the random data for US and save it to randomData database in table Agents
-// got('http://pplapi.com/country/us/random.json')
-//     .then(response => {
-//     body =JSON.parse(response.body);
-//     var userDetails = {
-//           neuroticism: body.neuroticism,
-//           language: body.language,
-//           country_tld: body.country_tld,
-//           age: body.age,
-//           income: body.income,
-//           longitude: body.longitude,
-//           sex: body.sex,
-//           religion: body.religion,
-//           extraversion: body.extraversion,
-//           date_of_birth: body.date_of_birth,
-//           agreeableness: body.agreeableness,
-//           id_str: body.id_str,
-//           conscientiousness: body.conscientiousness,
-//           internet: body.internet,
-//           country_name :body.country_name,
-//           openness: body.openness,
-//           id: body.id
-//         };
-//     })
-//     .catch(error => {
-//         console.log(error.response.body);
-//     });
-
-// // Use got to call the API http://pplapi.com/country/de/random.json to get the random data for Germany and save it to randomData database in table Agents
-// got('http://pplapi.com/country/de/random.json')
-//     .then(response => {
-//     body =JSON.parse(response.body);
-//     var userDetails = {
-//           neuroticism: body.neuroticism,
-//           language: body.language,
-//           country_tld: body.country_tld,
-//           age: body.age,
-//           income: body.income,
-//           longitude: body.longitude,
-//           sex: body.sex,
-//           religion: body.religion,
-//           extraversion: body.extraversion,
-//           date_of_birth: body.date_of_birth,
-//           agreeableness: body.agreeableness,
-//           id_str: body.id_str,
-//           conscientiousness: body.conscientiousness,
-//           internet: body.internet,
-//           country_name :body.country_name,
-//           openness: body.openness,
-//           id: body.id
-//         };
-//     })
-//     .catch(error => {
-//         console.log(error.response.body);
-//     });
-
-// // Use got to call the API http://pplapi.com/country/fr/random.json to get the random data for France and save it to randomData database in table Agents
-// got('http://pplapi.com/country/fr/random.json')
-//     .then(response => {
-//     body =JSON.parse(response.body);
-//     var userDetails = {
-//           neuroticism: body.neuroticism,
-//           language: body.language,
-//           country_tld: body.country_tld,
-//           age: body.age,
-//           income: body.income,
-//           longitude: body.longitude,
-//           sex: body.sex,
-//           religion: body.religion,
-//           extraversion: body.extraversion,
-//           date_of_birth: body.date_of_birth,
-//           agreeableness: body.agreeableness,
-//           id_str: body.id_str,
-//           conscientiousness: body.conscientiousness,
-//           internet: body.internet,
-//           country_name :body.country_name,
-//           openness: body.openness,
-//           id: body.id
-//         };
-//     })
-//     .catch(error => {
-//         console.log(error.response.body);
-//     });
-// // Use got to call the API http://pplapi.com/country/fr/random.json to get the random data for Russia and save it to randomData database in table Agents
-// got('http://pplapi.com/country/ru/random.json')
-//     .then(response => {
-//     body =JSON.parse(response.body);
-//     var userDetails = {
-//           neuroticism: body.neuroticism,
-//           language: body.language,
-//           country_tld: body.country_tld,
-//           age: body.age,
-//           income: body.income,
-//           longitude: body.longitude,
-//           sex: body.sex,
-//           religion: body.religion,
-//           extraversion: body.extraversion,
-//           date_of_birth: body.date_of_birth,
-//           agreeableness: body.agreeableness,
-//           id_str: body.id_str,
-//           conscientiousness: body.conscientiousness,
-//           internet: body.internet,
-//           country_name :body.country_name,
-//           openness: body.openness,
-//           id: body.id
-//         };
-//     })
-//     .catch(error => {
-//         console.log(error.response.body);
-//     });
-// // Use got to call the API http://pplapi.com/country/fr/random.json to get the random data for Japan and save it to randomData database in table Agents
-//     got('http://pplapi.com/country/jp/random.json')
-//     .then(response => {
-//     body =JSON.parse(response.body);
-//     var userDetails = {
-//           neuroticism: body.neuroticism,
-//           language: body.language,
-//           country_tld: body.country_tld,
-//           age: body.age,
-//           income: body.income,
-//           longitude: body.longitude,
-//           sex: body.sex,
-//           religion: body.religion,
-//           extraversion: body.extraversion,
-//           date_of_birth: body.date_of_birth,
-//           agreeableness: body.agreeableness,
-//           id_str: body.id_str,
-//           conscientiousness: body.conscientiousness,
-//           internet: body.internet,
-//           country_name :body.country_name,
-//           openness: body.openness,
-//           id: body.id
-//         };
-//     })
-//     .catch(error => {
-//         console.log(error.response.body);
-//     });
+async function getDetailsByCode(){
+try{
+ var code =[
+   'us',
+   'fr',
+   'de',
+   'ru',
+   'jp'
+ ]
+ console.log('code',code);
+ for(i=0; i<code.length;i++){
+ var detailsByCode = await superagent.get('http://pplapi.com/country/' + code[i] + '/random.json');
+ var responseByCode = detailsByCode.body;
+ var insertedByCode = await dbData.db('randomdata').table('datadetails').insert(responseByCode).run();
+ }
+ return insertedByCode;
+}
+ catch(e){
+   console.log(e)
+ }
+}
+getDetailsByCode().then(function(DataByCode){
+     console.log('DataByCode',DataByCode);
+})
