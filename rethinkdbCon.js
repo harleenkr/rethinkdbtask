d6ad1f752b66cf9882317c1786090e731012bb84
Harleen Kaur <harleen.kaur212@gmail.com>
2017-08-25 10:52:50 +0530

    type(fix): removed unwanted code

--- rethinkdbCon.js
+++ rethinkdbCon.js
@@ -1,7 +1,18 @@
-// require the rethinkdbdash Module
-var r = require('rethinkdbdash')();
-
-//save the data into a table called as 'agents'
-r.table('agents').run().then(function(result) {
-  console.log("result", result);
-});
+const reThinkPlus = require('rethink-plus')
+var superagent = require('superagent')
+const dbData = new reThinkPlus({
+  })
+async function getDetails(){
+try{
+  var details = await superagent.get('http://pplapi.com/random.json');
+  var response = details.body;
+   var inserted = await dbData.db('randomdata').table('datadetails').insert(response).run();
+  return inserted;
+}
+ catch(e){
+   console.log(e)
+ }
+}
+getDetails().then(function(Data){
+    //console.log('Data',Data);
+})
